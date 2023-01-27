751d25a98ee892d0c41919d29587cc90cae05521
Korode Ademoroti <122447344+PapaBeer01@users.noreply.github.com>
2023-01-27 23:26:06 +0100

    Delete main directory

--- main/Encryption.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-fernet = Fernet(key)
-
-def encrypt(n):
-    return fernet.encrypt(n.encode())
-
-def decrypt(password):
-    return fernet.encrypt(password.encode())
